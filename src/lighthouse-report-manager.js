b575f05f4d81559426eaef4c0f3e202782bfea69
Gustavo Bueno <gustavo.bueno89@gmail.com>
2018-10-02 10:18:53 +0200

    use map/filter/reducer instead of forEach loops

--- src/lighthouse-report-manager.js
+++ src/lighthouse-report-manager.js
@@ -5,53 +5,50 @@ const BIN_PATH = require.resolve('lighthouse/lighthouse-cli/index.js')
 module.exports = class LightHouseReportManager {
   static generate(paramUrls) {
     const urls = !Array.isArray(paramUrls) ? [paramUrls] : paramUrls;
-    let lighthouseReports = [];
-    urls.forEach(url => {
-      lighthouseReports.push(
-        new Promise((resolve, reject) => {
-          const child = spawn("node", [
-            BIN_PATH,
-            url,
-            "--output=json",
-            '--chrome-flags="--headless"'
-          ]);
-          let report = "";
-          let counter = 0;
-          // use child.stdout.setEncoding('utf8'); if you want text chunks
-          child.stdout.on("data", chunk => {
-            // data from standard output is here as buffers
-            report += chunk.toString();
-          });
 
-          child.on("close", code => {
-            if(report){
-            !code
-              ? resolve({
-                  report: JSON.parse(report)
-                })
-              : resolve({ report: JSON.parse(report) });
-              } else {
-                reject(new Error('No report was generated, this may be caused because the url can be accesed. Is your url a accesible?'))
-              }
-          });
-        })
-      );
-    });
+    const lighthouseReports = urls.map(url => {
+      return new Promise((resolve, reject) => {
+        const child = spawn("node", [
+          BIN_PATH,
+          url,
+          "--output=json",
+          '--chrome-flags="--headless"'
+        ]);
+        let report = "";
+        let counter = 0;
+        // use child.stdout.setEncoding('utf8'); if you want text chunks
+        child.stdout.on("data", chunk => {
+          // data from standard output is here as buffers
+          report += chunk.toString();
+        });
+
+        child.on("close", code => {
+          if(report){
+          !code
+            ? resolve({
+                report: JSON.parse(report)
+              })
+            : resolve({ report: JSON.parse(report) });
+            } else {
+              reject(new Error('No report was generated, this may be caused because the url can be accesed. Is your url a accesible?'))
+            }
+        });
+      })
+    })
 
     return lighthouseReports;
   }
 
   static parseReport(report, scoreThresholds) {
-    const parsedCategories = {};
     const categories = report.report.categories;
-    Object.keys(categories).forEach(categoryName => {
-      parsedCategories[categoryName] = {
+    const parsedCategories = Object.keys(categories).reduce((obj, categoryName) => {
+      obj[categoryName] = {
         score: categories[categoryName].score * 100,
         scoreThreshold: scoreThresholds[categoryName],
-        pass:
-          categories[categoryName].score * 100 >= scoreThresholds[categoryName]
-      };
-    });
+        pass: categories[categoryName].score * 100 >= scoreThresholds[categoryName]
+      }
+      return obj;
+    }, {})
 
     return {
       url: report.report.finalUrl,
